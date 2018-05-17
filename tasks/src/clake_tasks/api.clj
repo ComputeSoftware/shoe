54f8afd1284183b96d9d95a2aab793b0a38894b6
Kenny Williams <kennyjwilli@gmail.com>
2018-05-17 15:04:03 -0700

    Closer to a real task API.

--- tasks/src/clake_tasks/api.clj
+++ tasks/src/clake_tasks/api.clj
@@ -1,3 +1,19 @@
-(ns clake-tasks.api)
+(ns clake-tasks.api
+  (:require
+    [clojure.spec.alpha :as s]))
 
-()
+(s/def :clake/cli-opts vector?)
+(s/def ::attr-map (s/keys :req [:clake/cli-opts]))
+(s/def ::deftask-args (s/cat :docstring (s/? string?)
+                             :attr-map ::attr-map
+                             :argv vector?
+                             :body (s/* any?)))
+
+(defmacro deftask
+  [task-name & args]
+  (let [{:keys [docstring attr-map argv body]} (s/conform ::deftask-args args)]
+    `(defn ~task-name
+       ~@(when docstring [docstring])
+       ~(select-keys attr-map [:clake/cli-opts])
+       ~argv
+       ~@body)))
--- tasks/src/clake_tasks/built_in.clj
+++ tasks/src/clake_tasks/built_in.clj
@@ -1,10 +1,22 @@
 (ns clake-tasks.built-in
   (:require
     [clojure.main :as main]
-    [clojure.tools.nrepl.server :as nrepl-server]))
+    [clojure.tools.nrepl.server :as nrepl-server]
+    [clake-tasks.api :as api]))
 
-(defn nrepl
-  []
-  (let [repl (main/repl)
-        server (nrepl-server/start-server)]
-    (println "nREPL server started on port " (:port server))))
+(api/deftask nrepl
+  "Task that starts an nREPL server.
+
+  Note: This is a blocking task."
+  {:clake/cli-opts [["-p" "--port PORT" "Port to start nREPL server on."
+                     :default 0
+                     :parse-fn #(Integer/parseInt %)]
+                    ["-l" "--lein-port" "Spit the port to .nrepl-port."]]}
+  [{:keys [port lein-port] :as task-opts}]
+  (let [#_#_repl (main/repl)
+        server (nrepl-server/start-server :port port)
+        selected-port (:port server)]
+    (println (format "nREPL server started on port %s" selected-port))
+    (when lein-port
+      (spit ".nrepl-port" selected-port))
+    @(promise)))
