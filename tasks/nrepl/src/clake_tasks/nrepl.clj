39da0de34fd00e35cb068703a606cc0119368774
Kenny Williams <kennyjwilli@gmail.com>
2018-05-31 09:24:32 -0700

    Add log ns.

--- tasks/nrepl/src/clake_tasks/nrepl.clj
+++ tasks/nrepl/src/clake_tasks/nrepl.clj
@@ -1,13 +1,14 @@
 (ns clake-tasks.nrepl
   (:require
     [clojure.java.io :as io]
-    [clojure.tools.nrepl.server :as nrepl-server])
+    [clojure.tools.nrepl.server :as nrepl-server]
+    [clake-common.log :as log])
   (:import (java.nio.file Files)))
 
 (defn nrepl
   [{:keys [port lein-port] :as task-opts}]
   (let [#_#_repl (main/repl)
-        server (nrepl-server/start-server :port port)
+        server (nrepl-server/start-server :port 0)
         selected-port (:port server)]
     (log/info (format "nREPL server started on port %s" selected-port))
     (when lein-port
