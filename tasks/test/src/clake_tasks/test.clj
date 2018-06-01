a19f3db343e88c8a41198394e2030b9d53a3c339
Kenny Williams <kennyjwilli@gmail.com>
2018-05-31 17:44:25 -0700

    Move test over to new format.

---
+++ tasks/test/src/clake_tasks/test.clj
@@ -0,0 +1,18 @@
+(ns clake-tasks.test
+  (:require
+    [clojure.test :as clj-test]
+    [pjstadig.humane-test-output :as humane-test]
+    [clake-common.util :as util]
+    [clake-common.task :as task]))
+
+(defn test
+  "Run the project's tests."
+  {:clake/cli-specs []}
+  [{:keys [aliases]} {:clake/keys [deps-edn]}]
+  (humane-test/activate!)
+  (let [namespaces (util/namespaces-in-project deps-edn aliases)
+        ;; we need to make sure that all namespaces have been loaded
+        _ (apply require namespaces)
+        {:keys [fail]} (apply clj-test/run-tests namespaces)]
+    (when (not= 0 fail)
+      (task/exit false))))
