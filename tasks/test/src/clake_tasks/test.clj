b2c2a0bb28a5d3f2165c793fb73826930a245f19
Kenny Williams <kennyjwilli@gmail.com>
2018-06-04 18:02:17 -0700

    Use cognitect test-runner

--- tasks/test/src/clake_tasks/test.clj
+++ tasks/test/src/clake_tasks/test.clj
@@ -4,23 +4,21 @@
     [clojure.string :as str]
     [clojure.test :as clj-test]
     [pjstadig.humane-test-output :as humane-test]
+    [cognitect.test-runner :as test-runner]
     [clake-common.util :as util]
     [clake-common.task :as task]
-    [clake-common.shell :as shell]))
+    [clake-common.shell :as shell]
+    [clake-common.script.built-in-tasks :as tasks]))
 
 (defn test
   "Run the project's tests."
-  {:clake/cli-specs [["-a" "--aliases VALS" ""
-                      :parse-fn (fn [vals]
-                                  (str/split vals #","))]]}
-  [{:keys [aliases]}]
+  {:clake/cli-specs (tasks/cli-spec `test)}
+  [opts]
   (humane-test/activate!)
-  (let [deps-edn (shell/full-deps-edn (shell/deps-config-files))
-        namespaces (util/namespaces-in-project deps-edn aliases)
-        ; we need to make sure that all namespaces have been loaded
-        _ (apply require namespaces)
-        {:keys [fail]} (apply clj-test/run-tests namespaces)]
-    (when (not= 0 fail)
-      (shell/system-exit false))))
+  (try
+    (let [{:keys [fail error]} (test-runner/test opts)]
+      (when (or (not= 0 fail) (not= 0 error))
+        (shell/exit false)))
+    (finally (shutdown-agents))))
 
 (task/def-task-main test)
