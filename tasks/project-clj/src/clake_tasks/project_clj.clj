05446dee04a256845043418c33b6f0fcd7e86214
Kenny Williams <kenny@computesoftware.com>
2018-06-06 13:52:17 -0700

    Add :middleware to project-clj

--- tasks/project-clj/src/clake_tasks/project_clj.clj
+++ tasks/project-clj/src/clake_tasks/project_clj.clj
@@ -12,6 +12,7 @@
       (list
         'defproject (symbol project-name) "0.1.0"
         :plugins [['lein-tools-deps lein-tools-deps-version]]
+        :middleware ['lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
         :lein-tools-deps/config {:config-files [:install :user :project]}))))
 
 (defn project-clj
