ac976695646af85d8a11c71748b972c51cd4793c
Kenny Williams <kenny@computesoftware.com>
2018-06-06 13:26:57 -0700

    Remove useless project.clj keys.

--- tasks/project-clj/src/clake_tasks/project_clj.clj
+++ tasks/project-clj/src/clake_tasks/project_clj.clj
@@ -11,10 +11,6 @@
     (pp/pprint
       (list
         'defproject (symbol project-name) "0.1.0"
-        :description "FIXME: write description"
-        :url "http://example.com/FIXME"
-        :license {:name "Eclipse Public License"
-                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
         :plugins [['lein-tools-deps lein-tools-deps-version]]
         :lein-tools-deps/config {:config-files [:install :user :project]}))))
 
