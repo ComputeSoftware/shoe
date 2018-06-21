35bf255a1ae8178a6e16220a218785828496e4eb
Kenny Williams <kenny@computesoftware.com>
2018-06-21 08:25:10 -0700

    Start improving project.clj

--- tasks/project-clj/src/clake_tasks/project_clj.clj
+++ tasks/project-clj/src/clake_tasks/project_clj.clj
@@ -5,6 +5,14 @@
     [clojure.string :as str])
   (:import (java.io File)))
 
+(def cwd (System/getProperty "user.dir"))
+
+(defn lein-tools-deps-project-clj-params
+  [lein-tools-deps-version]
+  {:plugins                [['lein-tools-deps lein-tools-deps-version]]
+   :middleware             ['lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+   :lein-tools-deps/config {:config-files [:install :user :project]}})
+
 (defn generate-project-clj-string
   [project-name lein-tools-deps-version]
   (with-out-str
@@ -15,13 +23,22 @@
         :middleware ['lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
         :lein-tools-deps/config {:config-files [:install :user :project]}))))
 
+(defn project-clj-exists?
+  []
+  (.exists (io/file cwd "project.clj")))
+
+(defn project-name-from-parent-dir
+  []
+  (last (str/split cwd (re-pattern File/separator))))
+
 (defn project-clj
   [_]
-  (let [cwd (System/getProperty "user.dir")
-        parent-dir-name (last (str/split cwd (re-pattern File/separator)))
+  (let [parent-dir-name (project-name-from-parent-dir)
         project-string (generate-project-clj-string
                          parent-dir-name
                          "0.4.0-SNAPSHOT")]
+    (if (project-clj-exists?)
+      )
     (spit (io/file cwd "project.clj") project-string)))
 
 (defn -main
