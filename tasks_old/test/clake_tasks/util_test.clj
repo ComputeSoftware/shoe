a985c25d5139250ac88479f2d4e14ab63596ea79
Kenny Williams <kennyjwilli@gmail.com>
2018-06-21 15:41:40 -0700

    Remove old tasks.

--- tasks_old/test/clake_tasks/util_test.clj
+++
@@ -1,9 +0,0 @@
-(ns clake-tasks.util-test
-  (:require
-    [clojure.test :refer :all]
-    [clake-tasks.test-util :as t]
-    [clake-tasks.util :as util]))
-
-(deftest file-name-test
-  (t/with-fileset [a-edn ["a.edn" ""]]
-    (is (= "a.edn" (util/file-name a-edn)))))
