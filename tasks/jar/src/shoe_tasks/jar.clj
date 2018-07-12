325c9def43dbc9b75f797ad6ee4cb2b1cfffe41b
Kenny Williams <kenny@computesoftware.com>
2018-07-11 19:43:53 -0700

    Rewrite archive based tasks to use our fs ns instead of hara.

--- tasks/jar/src/shoe_tasks/jar.clj
+++ tasks/jar/src/shoe_tasks/jar.clj
@@ -4,57 +4,35 @@
     [clojure.java.io :as io]
     [shoe-common.log :as log]
     [shoe-common.fs :as fs]
-    [shoe-common.cli-utils :as cli-utils]
-    [shoe-tasks.archive :as archive-task]))
+    [shoe-common.cli-utils :as cli-utils]))
 
 (defn get-project-paths
   []
   (:paths (fs/get-cwd-deps-edn)))
-
-(defn manifestify-keyword
-  [k]
-  (str/join "-" (map str/capitalize (str/split (name k) #"-"))))
-
-;; https://docs.oracle.com/javase/6/docs/technotes/guides/jar/jar.html#Manifest%20Specification
-(defn manifest-from-map
-  [m]
-  (str (str/join "\n"
-                 (reduce-kv (fn [lines k v]
-                              ;; we ignore nil values
-                              (if (and v (not (str/blank? v)))
-                                (conj lines (format "%s: %s"
-                                                    (manifestify-keyword k)
-                                                    (str v)))
-                                lines))
-                            ;; Manifest-Version must come first
-                            ["Manifest-Version: 1.0"]
-                            m))
-       "\n"))
 
 (def default-manifest
   {:built-by   (System/getProperty "user.name")
    :created-by "Shoe"
    :build-jdk  (System/getProperty "java.version")})
 
-(def manifest-path "META-INF/MANIFEST.MF")
+(defn manifest-map
+  [manifest]
+  (merge default-manifest manifest))
 
-(defn write-manifest
-  [out-dir main]
-  (fs/spit-at-path (fs/path out-dir manifest-path)
-                   (manifest-from-map (merge default-manifest
-                                             {:main-class (str (munge main))}))))
-
-(defn write-jar-contents-directory
-  [jar-contents-path {:keys [main]}]
+(defn copy-project-paths
+  [copy-to-path]
   (let [project-paths (get-project-paths)
         _ (when (empty? project-paths) (log/warn ":paths is empty."))]
     ;; put project paths into jar dir
     (doseq [path project-paths]
-      (fs/copy path jar-contents-path))
-    ;; put manifest into jar dir
-    (write-manifest jar-contents-path main)
+      (fs/copy path copy-to-path))
     ;; TODO: write pom.xml
     ))
+
+(defn write-jar-contents-directory
+  [jar-contents-path]
+  ;; TODO: write pom.xml
+  (copy-project-paths jar-contents-path))
 
 (defn jar
   "Build a jar file from the project."
@@ -69,9 +47,8 @@
   (let [jar-contents-path (fs/path target jar-name)
         final-jar-path (fs/path target jar-name)]
     ;; put jar contents into directory
-    (write-jar-contents-directory jar-contents-path task-opts)
+    (write-jar-contents-directory jar-contents-path)
     ;; write jar
-    (archive-task/archive {:in  (str jar-contents-path)
-                           :out (str final-jar-path)})
+    (fs/jar jar-contents-path final-jar-path (manifest-map {:main (:main task-opts)}))
     (log/info "Cleaning up ...")
     (fs/delete jar-contents-path)))
--- tasks/libdir/src/shoe_tasks/libdir.clj
+++ tasks/libdir/src/shoe_tasks/libdir.clj
@@ -101,8 +101,12 @@
 (defn libdir
   {:shoe/cli-specs [["-o" "--out PATH" "Path to output the libs to."
                      :validate-fn (fn [path]
-                                    (.exists (io/file path)))
+                                    (not (fs/file? path)))
                      :default "libs"]]}
   [{:keys [out]}]
   (let [cp-vec (util/parse-classpath-string (shell/classpath-string-from-clj))]
+    #_(when (fs/exists? out)
+      (log/info out "already exists. Overwriting.")
+      (fs/delete out))
+    (fs/create-directories out)
     (explode-classpath cp-vec out)))
