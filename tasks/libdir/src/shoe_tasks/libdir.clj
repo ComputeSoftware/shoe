36437494cb5badc8f3120128aab7d39451bea0ea
Kenny Williams <kenny@computesoftware.com>
2018-07-19 09:34:49 -0700

    Change libdir to not explode jars.

--- tasks/libdir/src/shoe_tasks/libdir.clj
+++ tasks/libdir/src/shoe_tasks/libdir.clj
@@ -1,102 +1,14 @@
 (ns shoe-tasks.libdir
   (:require
-    [clojure.edn :as edn]
-    [clojure.string :as str]
-    [clojure.java.io :as io]
     [shoe-common.fs :as fs]
     [shoe-common.log :as log]
     [shoe-common.shell :as shell]
     [shoe-common.util :as util]))
 
-(defn trim-beginning-slash
-  [s]
-  (if (str/starts-with? s "/")
-    (subs s 1)
-    s))
-
-(defn file-name
-  [path]
-  (str (fs/relativize (fs/parent (fs/path path)) (fs/path path))))
-
-(defn excluded?
-  "Returns true of `filename` should be excluded from the JAR."
-  [filename]
-  (let [filename (trim-beginning-slash (str filename))]
-    (or (contains? #{"project.clj"
-                     "LICENSE"
-                     "COPYRIGHT"} filename)
-        (re-matches #"(?i)META-INF/.*\.(?:MF|SF|RSA|DSA)" filename)
-        (re-matches #"(?i)META-INF/(?:INDEX\.LIST|DEPENDENCIES|NOTICE|LICENSE)(?:\.txt)?" filename))))
-
-(defmulti copy-source (fn [src dest]
-                        (cond
-                          (fs/directory? src) :directory
-                          (and (fs/file? src)
-                               (re-find #"\.jar$" (str src))) :jar
-                          :else :default)))
-
-(defmethod copy-source :default
-  [src _]
-  (log/error (str "Could not copy classpath entry: " src)))
-
-(defmethod copy-source :directory
-  [src dest]
-  (let [all-files (fs/list src {:filter-fn (fn [path]
-                                             (and (fs/file? path)
-                                                  (not (excluded? path))))})
-        source-dest (map (fn [source-path]
-                           {:source source-path
-                            :dest   (fs/path dest (fs/relativize src source-path))})
-                         all-files)]
-    (doseq [{:keys [source dest]} source-dest]
-      (fs/copy source dest))))
-
-(defmethod copy-source :jar
-  [src dest]
-  (fs/extract-archive src dest {:filter-fn
-                                (fn [jar-path]
-                                  ;; remove the starting / from the jar paths
-                                  (and (fs/file? jar-path)
-                                       (-> jar-path str (subs 1) (excluded?) (not))))}))
-
-(defn merge-edn-files
-  [target-path f1 f2]
-  (spit target-path
-        (merge (edn/read-string (slurp f1))
-               (edn/read-string (slurp f2)))))
-
-(defn merge-files
-  [existing-path new-path]
-  (cond
-    (= "data_readers.clj" (file-name existing-path))
-    (merge-edn-files existing-path existing-path new-path)
-    ;(re-find #"^META-INF/services/" (trim-beginning-slash new-path))
-    :else (fs/move new-path existing-path {:overwrite? true})))
-
-(defn move-source-files
-  [source-dir target-dir]
-  (let [source-files (fs/list source-dir {:filter-fn fs/file?})]
-    (doseq [source-path source-files]
-      (let [target-path (fs/path target-dir (fs/relativize source-dir source-path))]
-        (if (fs/exists? target-path)
-          (merge-files target-path source-path)
-          (fs/move source-path target-path))))))
-
-(defn explode-classpath
-  "Creates a directory containing all files on the classpath. `classpath-vec is
-  a vector of classpath locations. `target` is the location to explode "
-  [classpath-vec target]
-  (let [temp-dirs (reduce (fn [acc src]
-                            (conj acc {:source src
-                                       :dest   (fs/create-temp-directory "shoe")}))
-                          [] classpath-vec)]
-
-    ;; copy all source files to temporary directories
-    (doseq [{:keys [source dest]} temp-dirs]
-      (copy-source source dest))
-    ;; move files one by one into the target directory, merging when applicable
-    (doseq [{:keys [dest]} temp-dirs]
-      (move-source-files dest target))))
+(defn copy-resources
+  [cp-vec target-dir]
+  (doseq [path cp-vec]
+    (fs/copy path target-dir {:recursive? true})))
 
 (defn libdir
   {:shoe/cli-specs [["-o" "--out PATH" "Path to output the libs to."
@@ -110,4 +22,4 @@
         (fs/delete out))
     (fs/create-directories out)
     (log/info "Copying classpath resources...")
-    (explode-classpath cp-vec out)))
+    (copy-resources cp-vec out)))
